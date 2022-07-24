6ca3251adc53de39c02c404f3521939ac55d4b2a
MusialK <musialkarolina98@gmail.com>
2022-07-24 12:39:40 +0200

    stworzenie html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+#tworzenie funkcji pobiera i zwraca wartość uzyskaną dzieki innej funkcji która renderuje szablon html
+
